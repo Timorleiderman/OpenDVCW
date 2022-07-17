5344645adcf557fb41981690f55d61fae4e558eb
Timor <Timorleiderman@gmail.com>
2022-07-17 07:36:24 +0300

    Update evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -4,12 +4,14 @@ import numpy as np
 import tensorflow as tf
 from math import log10, sqrt
 import OpenDVCW
+import subprocess
 
 
 class Evaluator(object):
     def __init__(self, bs=1, height=240, width=240, channels=3,
      input_seq_path="", workdir="", model_list=[], num_of_p_frames=7,
-      prefix="img", suffix=".png", bin_suffix=".bin", decom_prefix="decom") -> None:
+      prefix="im", suffix=".png", bin_suffix=".bin", decom_prefix="decom",
+       tave_path="/workspaces/OpenDVCW/cpp_encoder/build/tave", h264_workdir="", h265_workdir="") -> None:
         # bach size
         self.bs = bs
         self.height = height
@@ -28,6 +30,9 @@ class Evaluator(object):
         self.bin_suffix = bin_suffix
         self.decom_prefix = decom_prefix
 
+        self.tave_path = tave_path
+        self.h264_workdir = h264_workdir
+        self.h265_workdir = h265_workdir
         self.res = dict()
 
     def PSNR(self, original, compressed):
@@ -64,3 +69,22 @@ class Evaluator(object):
                 iter_res.append(self.test_tf_model(model, i_frame, p_frame, p_out_decom, p_frame_out_bin))   
 
             self.res[model] = iter_res             
+
+    def h264_test(self, bit_rate):
+        command = [self.tave_path, "libx264", str(bit_rate), self.input_seq_path, self.prefix, self.suffix, self.h264_workdir, str(self.num_of_p_frames)]
+        print("Command: ", command)
+        subprocess.run(command)
+
+        command = ["ffmpeg", "-i", self.h264_workdir + r"/output_joined.h264", self.h264_workdir + r"/decoded_%04d.png"]
+        subprocess.run(command)
+
+        for idx in np.arange(1, self.num_of_p_frames+1):
+                p_frame = os.path.join(self.input_seq_path, self.prefix + str(idx) + self.suffix)
+                p_frame_out_bin = os.path.join(self.h264_workdir, self.prefix + str(idx) + ".h264")
+                p_out_decom = os.path.join(self.h264_workdir, self.decom_prefix + str(idx) + self.suffix)
+
+
+    def h265_test(self, bit_rate):
+        # !/workspaces/OpenDVCW/cpp_encoder/build/tave libx265 1255555 "/mnt/WindowsDev/DataSets/Beauty_1920x1080_120fps_420_8bit_YUV_RAW/" "im" ".png" "/workspaces/OpenDVCW/cpp_encoder/build/workdir" 10
+        command = [self.tave_path, "libx265", bit_rate, self.input_seq_path, self.prefix, self.suffix, self.h265_workdir, self.num_of_p_frames]
+        subprocess.call(command)
